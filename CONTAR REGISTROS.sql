452c339ec2249a35676e799219bc7d4c0f496a8a
rpizarrog <rpizarro@itdurango.edu.mx>
2018-05-06 13:34:39 -0500

    Add files via upload

---
+++ CONTAR REGISTROS.sql
@@ -0,0 +1,14 @@
+select count(*) from categorias;
+select count(*) from productos;
+select count(*) from estados;
+select count(*) from municipios;
+select count(*) from tiposasentamientos;
+select count(*) from coloniasasentamientos;
+select count(*) from tiposclientes;
+select count(*) from clientes;
+select count(*) from tiendas;
+select count(*) from empleados;
+select count(*) from ventas;
+select count(*) from detalleventas;
+select count(*) from facturas;
+
